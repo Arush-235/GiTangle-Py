0e576c16decc9d2fc7c1566f7befa6623371694c
Arush Bartaria <arushbartaria@gmail.com>
2019-03-13 17:28:51 +0530

    added sudo

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ class setup(object):
     def init():
         self.is_IPFS, self.is_IRI, self.is_pystuff = False, False, False
         if not os.path.isdir("/usr/bin/GiTangle"):
-            os.system("mkdir /usr/bin/GiTangle")
+            os.system("sudo mkdir /usr/bin/GiTangle")
         if not os.path.isdir("/usr/bin/GiTangle/IRI"):
             self.is_IRI = True
         if not os.path.isdir("/usr/bin/GiTangle/IPFS"):
@@ -17,20 +17,20 @@ class setup(object):
     def install_IRI(self):
         if not self.is_IRI:
             os.system("cd ~ && cd /usr/bin/GiTangle")
-            os.system("mkdir IRI && cd IRI")
-            os.system("git clone https://github.com/iotaledger/iri")
+            os.system("sudo mkdir IRI && cd IRI")
+            os.system("sudo git clone https://github.com/iotaledger/iri")
             os.system("cd iri")
-            os.system("mvn clean compile")
-            os.system("mvn package")
+            os.system("sudo mvn clean compile")
+            os.system("sudo mvn package")
 
     def install_IPFS(self):
         if not self.is_IPFS:
             os.system("cd ~ && cd /usr/bin/GiTangle/")
-            os.system("mkdir IPFS")
+            os.system("sudo mkdir IPFS")
             os.system("cd IPFS")
-            os.system("wget https://dist.ipfs.io/go-ipfs/v0.4.19/go-ipfs_v0.4.19_linux-amd64.tar.gz")
-            os.system("tar xvfz go-ipfs.tar.gz")
-            os.system("cd go-ipfs")
+            os.system("sudo wget https://dist.ipfs.io/go-ipfs/v0.4.19/go-ipfs_v0.4.19_linux-amd64.tar.gz")
+            os.system("sudo tar xvfz go-ipfs.tar.gz")
+            os.system("sudo cd go-ipfs")
             os.system("./install.sh")
     
     def install_pystuff(self):
@@ -39,12 +39,12 @@ class setup(object):
             os.system("pip3 install ipfsapi")
             os.system("pip3 install github")
             os.system("pip3 install zerorpc")
-            os.system("mkdir pygit && cd pygit")
-            os.system("wget https://github.com/libgit2/libgit2/archive/v0.27.0.tar.gz")
-            os.system("tar xzf v0.27.0.tar.gz")
+            os.system("sudo mkdir pygit && cd pygit")
+            os.system("sudo wget https://github.com/libgit2/libgit2/archive/v0.27.0.tar.gz")
+            os.system("sudo tar xzf v0.27.0.tar.gz")
             os.system("cd libgit2-0.27.0/")
-            os.system("cmake .")
-            os.system("make")
+            os.system("sudo cmake .")
+            os.system("sudo make")
             os.system("sudo make install")
             os.system("pip3 install pygit2")
 
